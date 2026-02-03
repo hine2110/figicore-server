9a4035f78b8beef2ca7af6cff5df3b96742f0780
ngthanhdat-28 <89628604+ngthanhdat-28@users.noreply.github.com>
2026-02-03 20:16:01 +0700

    tạo import file excel tài khoản,tạo tài khoản nhân viên thành công gửi về mail kích hoạt, sửa lại profile nhân viên

--- src/employees/employees.module.ts
+++ src/employees/employees.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { EmployeesController } from './employees.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { MailModule } from '../mail/mail.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    MailModule,
+    JwtModule.registerAsync({
+        imports: [ConfigModule],
+        useFactory: async (configService: ConfigService) => ({
+          secret: configService.get<string>('JWT_SECRET') || 'FIGICORE_SECRET_KEY',
+          signOptions: { expiresIn: '1d' },
+        }),
+        inject: [ConfigService],
+      }),
+],
   controllers: [EmployeesController],
   providers: [EmployeesService],
 })
