3fb02d79a323efbfb49b28de57ad84cadec10173
rimrim2305 <nvhoaithuong2305@gmail.com>
2026-02-10 13:12:48 +0700

    ful chuc nang co ban Pos staff + UI/UX

--- src/pos/pos.module.ts
+++ src/pos/pos.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PosService } from './pos.service';
 import { PosController } from './pos.controller';
+import { PosOrdersController } from './pos-orders.controller';
+import { PosOrdersService } from './pos-orders.service';
 
 @Module({
-  controllers: [PosController],
-  providers: [PosService],
+  controllers: [PosController, PosOrdersController],
+  providers: [PosService, PosOrdersService],
 })
-export class PosModule {}
+export class PosModule { }
