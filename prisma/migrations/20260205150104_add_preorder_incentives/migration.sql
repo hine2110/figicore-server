e56432f69e3b8a7a23b65e46a1b78f63e5a9be97
hine2110 <huyntgde170695@fpt.edu.vn>
2026-02-15 13:14:21 +0700

    update mua hàng blindbox

--- prisma/migrations/20260205150104_add_preorder_incentives/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "preorder_contracts" ADD COLUMN     "is_full_payment" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "is_shipping_free" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "shipping_note" VARCHAR(255);
--- prisma/migrations/20260206012156_add_cart_payment_option/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "cart_items" ADD COLUMN     "payment_option" VARCHAR(20) NOT NULL DEFAULT 'DEPOSIT';
